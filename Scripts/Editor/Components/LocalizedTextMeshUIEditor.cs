d69990554fec99b32a8425718402e5885cbe19a5
Kleiner Hacker <Christoph.Pfeiffer.Berlin@freenet.de>
2022-01-09 22:36:19 +0100

    fix editor

---
+++ Scripts/Editor/Components/LocalizedTextMeshUIEditor.cs
@@ -0,0 +1,11 @@
+using UnityEditor;
+using UnityLocalization.Runtime.localization.Scripts.Runtime.Components;
+
+namespace UnityLocalization.Editor.localization.Scripts.Editor.Components
+{
+    [CustomEditor(typeof(LocalizedTextMeshUI))]
+    public sealed class LocalizedTextMeshUIEditor : LocalizedTextBaseEditor
+    {
+        
+    }
+}
