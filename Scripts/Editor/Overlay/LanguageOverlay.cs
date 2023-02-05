bf95ace9ae45f582bc0133abbf732a73f401639e
KleinerHacker <Christoph.Pfeiffer.Berlin@freenet.de>
2023-02-05 21:31:22 +0100

    change language preview selection into scene view overlay

---
+++ Scripts/Editor/Overlay/LanguageOverlay.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using UnityEditor;
+using UnityEditor.Overlays;
+using UnityEditor.Toolbars;
+using UnityEngine;
+using UnityLocalization.Runtime.localization.Scripts.Runtime;
+using Object = UnityEngine.Object;
+
+namespace UnityLocalization.Editor.localization.Scripts.Editor.Overlay
+{
+    [Overlay(typeof(SceneView), "Language")]
+    public class LanguageOverlay : ToolbarOverlay
+    {
+        public LanguageOverlay() : base("Localization/Language")
+        {
+        }
+    }
+
+    [EditorToolbarElement("Localization/Language", typeof(SceneView))]
+    public class LanguageButton : EditorToolbarButton
+    {
+        public LanguageButton()
+        {
+            tooltip = "Change view of shortcut input";
+            icon = (Texture2D)EditorGUIUtility.IconContent("d_Profiler.GlobalIllumination").image;
+            clicked += OnClicked;
+        }
+
+        private void OnClicked()
+        {
+            var ctxMenu = new GenericMenu();
+            ctxMenu.AddItem(new GUIContent("System Language"), UnityLocalize.CurrentLanguage == Application.systemLanguage, () =>
+            {
+                UnityLocalize.CurrentLanguage = Application.systemLanguage;
+            });
+            ctxMenu.AddSeparator(null);
+            foreach (var language in Enum.GetValues(typeof(SystemLanguage)).Cast<SystemLanguage>())
+            {
+                ctxMenu.AddItem(new GUIContent(language.ToString()), UnityLocalize.CurrentLanguage == language, () =>
+                {
+                    UnityLocalize.CurrentLanguage = language;
+                });                
+            }
+            ctxMenu.ShowAsContext();
+        }
+    }
+}
